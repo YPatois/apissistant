c5dc89b7ef7434223b83f20edba6864505659582
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2025-05-06 09:29:04 +0200

    Stuff

--- localdeepseekdl.py
+++ localdeepseekdl.py
@@ -3,6 +3,7 @@
 import os # Optional for faster downloading
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 #os.environ["HF_HOME"] = "some_path/.cache/huggingface"
+token=os.environ["HF_TOKEN"]
 
 from huggingface_hub import snapshot_download
 
@@ -11,8 +12,9 @@ while True:
     snapshot_download(
       repo_id = "unsloth/DeepSeek-R1-GGUF",
       local_dir = "DeepSeek-R1-GGUF",
-      allow_patterns = ["*UD-IQ1_S*"], # Select quant type UD-IQ1_S for 1.58bit
-      resume_download=True
+      allow_patterns = ["*UD-IQ1_S*"], # Select quant type UD-IQ1_S for 1.58bit,
+      token = token,
+      max_workers = 4
     )
   except RuntimeError:
     print ("Failed... Retrying...")
